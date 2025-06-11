cc6979aee8a9ddab652fdc1a17e08f74d4316d40
mohamed.brayant <k2019a@2023>
2025-06-11 23:28:08 +0100

    init project

---
+++ src/main/java/com/halloween/spring_gateway_example/SpringGatewayExampleApplication.java
@@ -0,0 +1,13 @@
+package com.halloween.spring_gateway_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewayExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewayExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/halloween/spring_gateway_example/SpringGatewayExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halloween.spring_gateway_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
